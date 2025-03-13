12241ff4a80fbc131ba1d5784fac92816202b7f2
WalidBenzina <walid.benzina.pro@gmail.com>
2025-03-14 00:42:01 +0100

    Cleaning

--- src/_models/client.model.ts
+++ src/_models/client.model.ts
@@ -1,7 +1,6 @@
 import { ClientStatus } from 'src/enums/client-status.enum'
 import { CommandeModel } from './commande.model'
 import { UserModel } from './user.model'
-import { offreDePrixModel } from './offre_de_prix.model'
 
 export interface ClientModel {
   idClient: number
@@ -12,7 +11,6 @@ export interface ClientModel {
   utilisateurs?: UserModel[]
   commandes?: CommandeModel[]
   statut: ClientStatus
-  offresDePrix?: offreDePrixModel[]
   createdAt: Date
   updatedAt: Date
 }
