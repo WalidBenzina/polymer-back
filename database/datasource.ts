cec72f395b907221d203963c3d3cef3fd0954742
WalidBenzina <walid.benzina.pro@gmail.com>
2025-03-17 01:31:05 +0100

    Fix Prod

--- database/datasource.ts
+++ database/datasource.ts
@@ -12,7 +12,7 @@ export const dataSourceOptions: DataSourceOptions = {
   database: process.env.DB_NAME || 'polymer_africa_db',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/database/migrations/*{.ts,.js}'],
-  synchronize: process.env.NODE_ENV === 'development',
+  synchronize: true,
   ssl: true,
   extra: {
     ssl: {
